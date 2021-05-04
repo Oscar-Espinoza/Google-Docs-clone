b99b20542660289de7f8c8ea643e0c5a393de6f7
unknown <oscardavid2094@gmail.com>
2021-05-04 18:23:06 -0300

    starting with Quill

---
+++ client/src/TextEditor.js
@@ -0,0 +1,11 @@
+import { useEffect } from 'react'
+import Quill from "quill"
+import "quill/dist/quill.snow.css"
+
+export default function TextEditor() {
+    useEffect(() => {
+        new Quill('#container', { theme: 'snow' })
+
+    }, [])
+    return <div id="container"></div>
+}
